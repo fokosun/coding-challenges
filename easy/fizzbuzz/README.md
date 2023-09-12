15f7eb0d7cc2aab27beb458404183277aeec1656
fokosun <okosunuzflorence@gmail.com>
2023-09-12 15:45:08 -0400

    update readme

--- easy/fizzbuzz/README.md
+++ easy/fizzbuzz/README.md
@@ -1,5 +1,7 @@
 ### Running tests
-Make sure you are in this directory `cd easy/fizzbuzz` then run:
+1) Make sure you are in this directory `cd easy/fizzbuzz`
+2) Implement your solution in the solution.go file
+3) Run the tests
 
 ```go
 go test fizzbuzz
--- easy/fizzbuzz/solution.go
+++ easy/fizzbuzz/solution.go
@@ -5,14 +5,17 @@ import (
 )
 
 func FizzBuzz(n int) string {
-	//fmt.Println(n)
-	if n%15 == 0 {
-		return "fizz buzz"
-	} else if n%5 == 0 {
-		return "buzz"
-	} else if n%3 == 0 {
-		return "fizz"
-	} else {
-		return strconv.Itoa(n)
-	}
+
+	//Try this solution then implement yours
+	//if n%15 == 0 {
+	//	return "fizz buzz"
+	//} else if n%5 == 0 {
+	//	return "buzz"
+	//} else if n%3 == 0 {
+	//	return "fizz"
+	//} else {
+	//	return strconv.Itoa(n)
+	//}
+
+	return strconv.Itoa(n) //remember to comment this
 }
