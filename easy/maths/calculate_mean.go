a0797cfa311c456ba4fc628ff8bf699109ab7611
fokosun <okosunuzflorence@gmail.com>
2023-09-12 17:48:45 -0400

    WIP math challenges

---
+++ easy/maths/calculate_mean.go
@@ -0,0 +1,14 @@
+package maths
+
+func CalculateMean(nums []float64) float64 {
+	length := float64(len(nums))
+	sum := 0.0
+
+	// Try to come up with your own solution!
+
+	//for i := range nums {
+	//	sum += nums[i]
+	//}
+
+	return sum / length
+}
